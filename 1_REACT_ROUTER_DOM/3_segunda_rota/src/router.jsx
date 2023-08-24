79f8a78a8d4f74656c059336fcb8b523f4bf3d80
Marcio Marçal <marciomarcal.dev@gmail.com>
2023-08-24 14:12:17 -0300

    segunda rota

--- 1_REACT_ROUTER_DOM/3_segunda_rota/src/router.jsx
+++ 1_REACT_ROUTER_DOM/3_segunda_rota/src/router.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom"
+
 import Home from "./pages/Home"
+import Usuarios from "./pages/Usuarios"
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/usuarios" element={<Usuarios />} />
         </Routes>
     )
 }
